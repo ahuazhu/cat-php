c76ef8fcf2ec8d6680c825f6c549b2be1cbed290
zhengwen.zhu <ahuazhu@gmail.com>
2016-07-19 22:46:48 +0800

    code format

--- Sender.php
+++ Sender.php
@@ -6,7 +6,7 @@ include_once("AtomicTransaction.php");
 $header = new Header();
 $transaction = new AtomicTransaction();
 
-$data = $header->encode(). $transaction->encode();
+$data = $header->encode() . $transaction->encode();
 
 $len = strlen($data);
 
@@ -17,7 +17,7 @@ $_data = $len_bin . $data_bin;
 
 echo $data;
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);  
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 $connection = socket_connect($socket, 'ahuazhu.com', 2280);    //连接服务器端socket  
 $result = socket_write($socket, $_data, strlen($_data));
 ?>
